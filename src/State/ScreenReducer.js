9c157c726db67be5246a39aae538443baf58c261
Davy Mitchell <daftspaniel@gmail.com>
2020-06-13 16:50:20 +0100

    Save and load state.

--- src/State/ScreenReducer.js
+++ src/State/ScreenReducer.js
@@ -1,13 +1,22 @@
 import { ACTIONS } from "./ScreenActions"
-const initialState = { primaryCharacter: 1, secondaryCharacter: 2 }
+const initialState = { primaryCharacter: 1, secondaryCharacter: 2, mode: "sg4" }
 
-export const screenReducer = function (state = initialState, action) {
+export const screenReducer = function (state, action) {
   switch (action.type) {
     case ACTIONS.SET_PRIMARY_CHAR:
-      return { ...state, primaryCharacter: action.newCharacter }
+      return save({ ...state, primaryCharacter: action.newCharacter })
     case ACTIONS.SET_SECONDARY_CHAR:
-      return { ...state, secondaryCharacter: action.newCharacter }
+      return save({ ...state, secondaryCharacter: action.newCharacter })
     default:
+      const isSavedState = localStorage.sgedit4State
+      state = isSavedState
+        ? JSON.parse(localStorage.sgedit4State)
+        : initialState
+
       return state
   }
+}
+const save = (newState) => {
+  localStorage.sgedit4State = JSON.stringify(newState)
+  return newState
 }
