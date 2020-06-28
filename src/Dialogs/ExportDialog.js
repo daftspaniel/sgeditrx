b5f71c668a66123fb899451e22040d9fe80dc69e
Davy Mitchell <daftspaniel@gmail.com>
2020-06-28 12:35:40 +0100

    Refactor dialog

--- src/Dialogs/ExportDialog.js
+++ src/Dialogs/ExportDialog.js
@@ -2,15 +2,8 @@ import React, { useState } from "react"
 import { useSelector } from "react-redux"
 
 import { makeStyles } from "@material-ui/core/styles"
-import { withStyles } from "@material-ui/core/styles"
 import Button from "@material-ui/core/Button"
 import Dialog from "@material-ui/core/Dialog"
-import MuiDialogTitle from "@material-ui/core/DialogTitle"
-import MuiDialogContent from "@material-ui/core/DialogContent"
-import MuiDialogActions from "@material-ui/core/DialogActions"
-import IconButton from "@material-ui/core/IconButton"
-import CloseIcon from "@material-ui/icons/Close"
-import Typography from "@material-ui/core/Typography"
 import FormControl from "@material-ui/core/FormControl"
 import Select from "@material-ui/core/Select"
 import TextField from "@material-ui/core/TextField"
@@ -19,50 +12,7 @@ import MenuItem from "@material-ui/core/MenuItem"
 import { generateCSV } from "../Lib/generateCSV"
 import { generateBASIC } from "../Lib/GenerateBasic"
 import { generateASM } from "../Lib/GenerateASM"
-
-const styles = (theme) => ({
-  root: {
-    margin: 0,
-    padding: theme.spacing(2),
-  },
-  closeButton: {
-    position: "absolute",
-    right: theme.spacing(1),
-    top: theme.spacing(1),
-    color: theme.palette.grey[500],
-  },
-})
-
-const DialogTitle = withStyles(styles)((props) => {
-  const { children, classes, onClose, ...other } = props
-  return (
-    <MuiDialogTitle disableTypography className={classes.root} {...other}>
-      <Typography variant="h6">{children}</Typography>
-      {onClose ? (
-        <IconButton
-          aria-label="close"
-          className={classes.closeButton}
-          onClick={onClose}
-        >
-          <CloseIcon />
-        </IconButton>
-      ) : null}
-    </MuiDialogTitle>
-  )
-})
-
-const DialogContent = withStyles((theme) => ({
-  root: {
-    padding: theme.spacing(2),
-  },
-}))(MuiDialogContent)
-
-const DialogActions = withStyles((theme) => ({
-  root: {
-    margin: 0,
-    padding: theme.spacing(1),
-  },
-}))(MuiDialogActions)
+import { DialogTitle, DialogContent, DialogActions } from "./DialogCommon"
 
 const useStyles = makeStyles((theme) => ({
   exportType: {
